16046a119e128e172061d33ac3cf59fd5f820d2a
pbonte92 <97885844+pbonte92@users.noreply.github.com>
2022-03-15 18:51:13 +0100

    5e commit
    Finalisation du code Python
    Sortie du model
    création du dashboard
    création de l'api

---
+++ app.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  3 16:28:37 2022
+
+@author: Paul.BONTE
+"""
+
+# -*- coding: utf-8 -*-
+import numpy as np
+import pickle
+import pandas as pd
+import json
+import lime
+from lime import lime_tabular
+from flask import Flask, request, jsonify, render_template
+
+
+app = Flask(__name__)
+
+path = "C:/Users/paul.bonte/Formation OC/P7_Bonte_Paul/P7_Paul_Bonte"
+data = pd.read_csv(path + "/data.csv")
+model = pickle.load(open(path + "/model_credit.pkl","rb"))
+exp = data.drop(columns = ["SK_ID_CURR"])
+
+def filter_dataset(df, client_id):
+    'Filters dataset down to a single line, being the chosen client ID'
+    X = df[df['SK_ID_CURR'] == int(client_id)]
+    X.drop(columns = ['SK_ID_CURR'], inplace = True)
+    return X
+
+
+def get_prediction(model , X):
+    resultat = model.predict_proba(X)[0]
+    return resultat
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    input = request.get_json(force= True)
+    id_client = input["id_client"]
+    X_id =filter_dataset(data , id_client)
+    prediction = get_prediction(model , X_id)
+
+    # Return output
+    return jsonify({'Proba class 0' : str(round(prediction[0]*100,2)) , 'Proba class 1' : str(round(prediction[1]*100,2))})
+
+@app.route('/lime' , methods=['POST'])
+def explain() : 
+    
+    input = request.get_json(force= True)
+    id_client = input["id_client"]
+    explainer = lime_tabular.LimeTabularExplainer(
+    training_data=np.array(exp),
+    feature_names= exp.columns,
+    class_names=['0', '1'],
+    mode='classification')
+    explained = explainer.explain_instance(
+    data_row=filter_dataset(data , id_client).iloc[0], 
+    predict_fn=model.predict_proba)
+    
+    return explained
+    
+
+if __name__ == "__main__":
+    app.run()
---
+++ dashboard.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import json
+from flask import Flask, request, jsonify, render_template
+
+
+path = "C:/Users/paul.bonte/Formation OC/P7_Bonte_Paul/P7_Paul_Bonte"
+df = pd.read_csv((path + "/train.csv"))
+valid_ids = df['SK_ID_CURR'].values.tolist()
+cols_no_graph = df[['SK_ID_CURR', 'TARGET']]
+df_columns = df.columns
+df_small = df.head(100)
+
+
+
+def filter_dataset():
+    'Filters dataset down to a single line, being the chosen client ID'
+    df_small = df[df['SK_ID_CURR'] == int(client_id)]
+    df_small.drop(columns = ['SK_ID_CURR'], inplace = True)
+    return df_small
+
+
+def get_prediction():
+    
+        url = 'http://127.0.0.1:5000/predict'
+        param = {'id_client' : client_id}
+        # Post JSON file
+        r = request.post(url, json = param)
+        # Visualize response
+        st.write(r)
+
+
+
+st.title("Credit risk assesment dashboard")
+
+client_id = st.text_input('Client ID:')
+if client_id == '':
+        st.write('Please enter a Client ID.')
+else:
+       if int(client_id) not in valid_ids:
+            st.markdown(':exclamation: This ID does not exist. Please enter a valid one.')
+       else:
+            # Get prediction
+            pred = get_prediction()
+            st.bar_chart(pred)
+           
+select = st.selectbox('choose variable 1',df_columns)
+feature_data = df_small[select]
+st.bar_chart(feature_data)
+
+select_2 = st.selectbox('choose variable 2',df_columns)
+feature_data_2 = df_small[select_2]
+st.bar_chart(feature_data_2)
+
+select_3 = st.selectbox('choose variable X',df_columns)
+feature_data_3 = df_small[select_3]
+select_4 = st.selectbox('choose variable Y',df_columns)
+feature_data_4 = df_small[select_4]
+data_graph = pd.concat([feature_data_3 , feature_data_4] , axis = 1)
+
+st.bar_chart(data_graph)
+
