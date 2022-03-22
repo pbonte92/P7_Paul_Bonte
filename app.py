2b9301a91411842899c177b6d4073b372302b655
pbonte92 <97885844+pbonte92@users.noreply.github.com>
2022-03-22 22:49:56 +0100

    Finalisation API / Dashboard

--- app.py
+++ app.py
@@ -12,15 +12,22 @@ import pandas as pd
 import json
 import lime
 from lime import lime_tabular
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
 
 path = "C:/Users/paul.bonte/Formation OC/P7_Bonte_Paul"
+path2 = "C:/Users/paul.bonte/Formation OC/P7_Bonte_Paul/P7_Paul_Bonte"
 data = pd.read_csv(path + "/data.csv")
-model = pickle.load(open(path + "/model_credit.pkl","rb"))
+model = pickle.load(open(path2 + "/model_credit.pkl","rb"))
 exp = data.drop(columns = ["SK_ID_CURR"])
+
+explainer = lime_tabular.LimeTabularExplainer(
+    training_data=np.array(exp),
+    feature_names= exp.columns,
+    class_names=['0', '1'],
+    mode='classification')
 
 def filter_dataset(df, client_id):
     'Filters dataset down to a single line, being the chosen client ID'
@@ -43,23 +50,29 @@ def predict():
     prediction = get_prediction(model , X_id)
 
     # Return output
-    return jsonify({'Proba class 0' : str(round(prediction[0]*100,2)) , 'Proba class 1' : str(round(prediction[1]*100,2))})
+    return jsonify({'Proba non défaut' : [float(round(prediction[0]*100,2)),int(50)] , 'Proba défaut' : [float(round(prediction[1]*100,2)),int(50)]})
 
 @app.route('/lime' , methods=['POST'])
 def explain() : 
     
     input = request.get_json(force= True)
     id_client = input["id_client"]
-    explainer = lime_tabular.LimeTabularExplainer(
-    training_data=np.array(exp),
-    feature_names= exp.columns,
-    class_names=['0', '1'],
-    mode='classification')
     explained = explainer.explain_instance(
     data_row=filter_dataset(data , id_client).iloc[0], 
     predict_fn=model.predict_proba)
+    explained = explained.as_list()
+    explained = pd.DataFrame(explained)
+    explained[0] = explained[0].str.replace('\<= 0.00', '')
+    explained[0] = explained[0].str.replace('\<=', '')
+    explained[0] = explained[0].str.replace('\>', '')
+    explained[0] = explained[0].str.replace('\<', '')
+    explained[0] = explained[0].str.replace('\>=', '')
+    #json_data = explained.to_json(orient='values')
+    explained = pd.DataFrame.to_dict(explained)
+      
+    return jsonify(explained)
     
-    return explained
+    
     
 
 if __name__ == "__main__":
--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
-import matplotlib as plt
-import seaborn as sns
-import json
-from flask import Flask, request, jsonify, render_template
+#import matplotlib as plt
+import matplotlib.pyplot as plt
+#from flask import Flask, request, jsonify, render_template
+import requests
 
 
 path = "C:/Users/paul.bonte/Formation OC/P7_Bonte_Paul"
@@ -11,9 +12,17 @@ df = pd.read_csv((path + "/train.csv"))
 valid_ids = df['SK_ID_CURR'].values.tolist()
 cols_no_graph = df[['SK_ID_CURR', 'TARGET']]
 df_columns = df.columns
-df_small = df.head(100)
+df_columns_num = df.select_dtypes(exclude=["bool_","object_"]).columns
+df_columns_cat = df.select_dtypes(exclude="number").columns
 
 
+def example_ids():
+    'Outputs 5 sample IDs of default and non-default clients'
+    sample = df['SK_ID_CURR'].sample(5).tolist()
+    for i in range(0, len(sample)): 
+        sample[i] = int(sample[i])
+    st.write("Examples of client IDs:")
+    st.write(str(sample).replace('[','').replace(']', ''))
 
 def filter_dataset():
     'Filters dataset down to a single line, being the chosen client ID'
@@ -22,15 +31,31 @@ def filter_dataset():
     return df_small
 
 
-def get_prediction():
+def get_prediction(client_id):
     
         url = 'http://127.0.0.1:5000/predict'
         param = {'id_client' : client_id}
         # Post JSON file
-        r = request.post(url, json = param)
+        r = requests.post(url, json = param)
         # Visualize response
-        st.write(r)
+        #df_pred = pd.json_normalize(r.json())
+        df_pred = pd.DataFrame.from_dict(r.json(), orient='index').transpose()
+        ax = df_pred.iloc[1].plot(kind ='line' , color = 'red')
+        df_pred[['Proba non défaut','Proba défaut']].iloc[0].plot(kind='bar', ax=ax)
+        return st.pyplot(ax.figure)
+        
+def get_explainer(client_id):
+    
+        url = 'http://127.0.0.1:5000/lime'
+        param = {'id_client' : client_id}
+        # Post JSON file
+        r = requests.post(url, json = param)
+        # Visualize response
+        df_exp = pd.DataFrame.from_dict(r.json(), orient='columns')
+        df_exp.set_index(list(df_exp)[0], inplace = True)
+        ax = df_exp.plot(kind ='barh')
 
+        return st.pyplot(ax.figure , clear_figure = True)
 
 
 st.title("Credit risk assesment dashboard")
@@ -38,27 +63,46 @@ st.title("Credit risk assesment dashboard")
 client_id = st.text_input('Client ID:')
 if client_id == '':
         st.write('Please enter a Client ID.')
+        example_ids()
+        
 else:
        if int(client_id) not in valid_ids:
             st.markdown(':exclamation: This ID does not exist. Please enter a valid one.')
        else:
             # Get prediction
-            pred = get_prediction()
-            st.bar_chart(pred)
-           
-select = st.selectbox('choose variable 1',df_columns)
-feature_data = df_small[select]
-st.bar_chart(feature_data)
+            pred = get_prediction(client_id)
+            exp = get_explainer(client_id)
+            
+            client_index = df[df['SK_ID_CURR']== int(client_id)].index.tolist()
+            
+            
+            select = st.selectbox('Choisissez une variable numérique',df_columns_num.drop(cols_no_graph))
+            feature_data = df[select]
+            aa = df[select].iloc[client_index]
+            ax = feature_data.plot(kind ='hist')
+            st.write('La valeur pour le client est :', aa.iloc[0])
+            st.pyplot(ax.figure, clear_figure = True)
 
-select_2 = st.selectbox('choose variable 2',df_columns)
-feature_data_2 = df_small[select_2]
-st.bar_chart(feature_data_2)
+            select_2 = st.selectbox('Choisissez une variable catégorielle',df_columns_cat)
+            feature_data_2 = df[select_2]
+            df_cat = feature_data_2.value_counts()
+            ax = df_cat.plot(kind="pie")
+            bb = df[select_2].iloc[client_index]
+            st.write('La valeur pour le client est :', bb.iloc[0])
+            st.pyplot(ax.figure, clear_figure = True)
 
-select_3 = st.selectbox('choose variable X',df_columns)
-feature_data_3 = df_small[select_3]
-select_4 = st.selectbox('choose variable Y',df_columns)
-feature_data_4 = df_small[select_4]
-data_graph = pd.concat([feature_data_3 , feature_data_4] , axis = 1)
-
-st.bar_chart(data_graph)
+            select_3 = st.selectbox('Choisissez une variable X',df_columns.drop(cols_no_graph))
+            feature_data_3 = df[select_3]
+            cc = df[select].iloc[client_index]
+            select_4 = st.selectbox('Choisissez une variable Y',df_columns.drop(cols_no_graph))
+            feature_data_4 = df[select_4]
+            dd = df[select].iloc[client_index]
+            x = np.array(feature_data_3)
+            y = np.array(feature_data_4)
+            ax = plt.scatter(x,y)
+            #ax = data_graph.plot(kind = 'scatter', x = data_graph['AMT_CREDIT'] , y = data_graph['CNT_CHILDREN'])
+            #st.dataframe(data_graph)
+            st.write('La valeur X pour le client est :', cc.iloc[0])
+            st.write('La valeur Y pour le client est :', dd.iloc[0])
+            st.pyplot(ax.figure)
 
