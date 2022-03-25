a7808c7213bfd65a0629d7607e4f4678ad7c4234
pbonte92 <97885844+pbonte92@users.noreply.github.com>
2022-03-25 21:24:23 +0100

    last submit

--- API/app.py
+++ API/app.py
@@ -83,5 +83,6 @@ def explain() :
     
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT" , 5000)
-    app.run(debug= False , host="0.0.0.0" , port = port)
+    #port = os.environ.get("PORT" , 5000)
+    #debug= False , host="0.0.0.0" , port = port
+    app.run()
